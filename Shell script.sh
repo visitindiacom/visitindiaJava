f9fe5f0cbcfbdd093cf38d31a649222f47a2a010
alldevopscloud <119701798+alldevopscloud@users.noreply.github.com>
2023-01-06 20:35:05 +0530

    uploaded to Server

---
+++ Shell script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash      
+#title           : hello.sh
+#description     : 
+#author		     : Sohail Shaikh
+#date            : 14.07.2022
+#version         : 2.0    
+#usage		     : sh hello.sh
+
+echo "Hello Guys"
+echo "Welcome to ShellScript"
+echo "This is the first shell script example !!"
+echo "Today date is"
+date
+echo "Shellscript is very easy"
+
+
+
